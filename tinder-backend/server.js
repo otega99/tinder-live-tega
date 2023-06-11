1d39621dee20c42d1c4846c493ee158af3208f15
otega99 <83589929+otega99@users.noreply.github.com>
2023-06-11 20:00:43 +0000

    connected to mongo db

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Cards from './dbCards.js'
+import dbCards from './dbCards.js'
 
 // App Config
 const app=express()
@@ -9,13 +11,30 @@ const connection_url=`mongodb+srv://oghenetegaakpowenre:KkaIHd1FptwRmEwq@cluster
 // Middlewares
 
 // DB Config
-mongoose.connect(connection_url,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology:true
-})
+mongoose.connect(connection_url)
 // API Endpoints
 app.get('/',(req,res)=> res.status(200).send('hello world'))
+app.post('/tinder/cards',(req,res)=>{
+    const dbCard=req.body
 
+    Cards.create(dbCard, (err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+})
+app.get('/tinder/cards',(req,res)=>{
+    const dbCard=req.body
+
+    Cards.find((err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
 // Listener
 app.listen(port,(req,res)=>console.log(`listening on localhost ${port}`))
